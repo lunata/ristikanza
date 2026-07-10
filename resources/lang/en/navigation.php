6933ef77a1351c3cb66ed43e37cd32e89c96b458
lunata <nataly@krc.karelia.ru>
2026-07-10 11:50:25 +0300

    language-switch

--- resources/lang/en/navigation.php
+++ resources/lang/en/navigation.php
@@ -1,16 +1,18 @@
 <?php
 return [
-    'about' => 'About the project',
+    'about' => 'About',
     'bible' => 'Biblical texts',
     'ethnography' => 'Ethnography',
     'folklore' => 'Folklore',
     'library' => 'Library',
-    'map' => 'Map of the Festive Culture of Southern Karelia',
+    'map' => 'Map',
+    'map_full' => 'Map of the Festive Culture of Southern Karelia',
     'monuments' => 'Written monuments',
     'oikonyms' => 'Oikonyms',
     'oikonyms_full' => 'Oikonyms of Northern Ladoga',
     'participants' => 'Participants',
     'publications' => 'Publications',
-    'textbooks' => 'School textbooks of Karelia',
+    'textbooks' => 'Textbooks',
+    'textbooks_full' => 'School textbooks of Karelia',
     'texts' => 'Texts',
 ];
