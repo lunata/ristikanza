8108c580e813221e06e74354ce1af3e893284e2c
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2026-07-26 16:55:21 +0300

    small

--- resources/views/includes/objs_on_map.blade.php
+++ resources/views/includes/objs_on_map.blade.php
@@ -6,7 +6,7 @@
 
     <script>
       // initialize Leaflet
-      var map = L.map('mapid').setView({lon: '{{empty($lon) ? '33' : $lon}}' , lat: '{{empty($lat) ? '63.5' : $lat}}}', {{empty($zoom) ? '7' : $zoom}});
+      var map = L.map('mapid').setView({lon: '{{empty($lon) ? '33' : $lon}}' , lat: '{{empty($lat) ? '63.5' : $lat}}'}, {{empty($zoom) ? '7' : $zoom}});
 
       @foreach ($objs->groupBy('color') as $color => $tmp)
       var {{ $color }}Icon = L.icon({
