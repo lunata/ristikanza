a0df5294e843e9f974ee046690cd1f104f1b3f43
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2026-07-26 17:02:27 +0300

    float

--- resources/views/oikonyms/on_map.blade.php
+++ resources/views/oikonyms/on_map.blade.php
@@ -4,12 +4,12 @@
 @section('h1', __('navigation.oikonyms_full'))
 
 @section('headExtra')
-    {!! css('select2.min') !!}  
+    {!! css('select2.min') !!}
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
          crossorigin=""/>
-    {!! css('map') !!}  
-    {!! css('leaflet') !!}  
+    {!! css('map') !!}
+    {!! css('leaflet') !!}
 @stop
 
 @section('search_form')
@@ -19,8 +19,8 @@
     @if ($meta['show_count'] == $meta['total_rec'])
             @include('includes.found_records', ['n_records'=>number_format($meta['total_rec'])])
     @else
-        <p>{!! __('oikonym.found_from', 
-            ['show_count'=>number_format($meta['show_count'], 0, ',', ' '), 
+        <p>{!! __('oikonym.found_from',
+            ['show_count'=>number_format($meta['show_count'], 0, ',', ' '),
              'total'=>number_format($meta['total_rec'], 0, ',', ' ')]) !!}
         </p>
     @endif
@@ -28,19 +28,19 @@
          <div class="col-sm-6 output_in">
             @if ($meta['total_rec'])
             <a href="{{ route('oikonyms.index').search_values_by_URL($url_args) }}">{!! __('oikonym.back_to_index') !!}</a>
-            @endif 
+            @endif
          </div>
     </div>
 @endsection
 
-@section('wide-block')   
+@section('wide-block')
     @if (empty($url_args['only_exact_coords']))
     <div class="row" style="margin-bottom: 20px;">
-        <div class="col-sm-4"><img src="/img/markers/marker-icon-blue.png" class="legend-icon"> 
+        <div class="col-sm-4"><img src="/img/markers/marker-icon-blue.png" class="legend-icon">
             {{ __('oikonym.coord_toponym') }}</div>
-        <div class="col-sm-4"><img src="/img/markers/marker-icon-grey.png" class="legend-icon"> 
+        <div class="col-sm-4"><img src="/img/markers/marker-icon-grey.png" class="legend-icon">
             {{ __('oikonym.coord_settl') }}</div>
-        <div class="col-sm-4"><img src="/img/markers/marker-icon-violet.png" class="legend-icon"> 
+        <div class="col-sm-4"><img src="/img/markers/marker-icon-violet.png" class="legend-icon">
             {{ __('oikonym.coord_toponyms') }}</div>
     </div>
     @endif
@@ -52,8 +52,8 @@
         {!! js('lists') !!}
         {!! js('special_symbols') !!}
         @include('includes.objs_on_map'.(empty($url_args['not_claster']) ? '_claster' : ''), [
-            'lon' => $url_args['min_lon']+($url_args['max_lon']-$url_args['min_lon'])/2,
-            'lat' => $url_args['min_lat']+($url_args['max_lat']-$url_args['min_lat'])/2,
+            'lon' => (float)$url_args['min_lon']+($url_args['max_lon']-$url_args['min_lon'])/2,
+            'lat' => (float)$url_args['min_lat']+($url_args['max_lat']-$url_args['min_lat'])/2,
             'bounds' => $meta['bounds']])
 @endsection
 
@@ -72,7 +72,7 @@
             placeholder: '{{ trans('oikonym.district') }}',
             width: '100%'
         });
-        
+
         $('input[type=reset]').on('click', function (e) {
         @foreach (['districts', 'settlements', 'districts1926', 'selsovets1926', 'settlements1926', 'sources'] as $f)
             $('#search_{{ $f }}').val(null).trigger('change');
@@ -86,5 +86,5 @@
         @endforeach
         });
             $('#search_toponym').attr('value','');
-        });        
+        });
 @stop
