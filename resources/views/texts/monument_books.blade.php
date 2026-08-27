d7a7e95cec109db680a5b0525eabd1184deebc21
Andrew Krizhanovsky <andrew.krizhanovsky@gmail.com>
2026-08-28 01:51:37 +0300

    book style

--- resources/views/texts/monument_books.blade.php
+++ resources/views/texts/monument_books.blade.php
@@ -11,10 +11,12 @@
     <div class="row">
     @foreach ($books as $book_id => $book_info)
         <div class="col-sm-3">
+            <div class="book-b">
         @if ($book_info['photo'])
-            <img class='photo' src="{{ config('services.dictorpus.url').$book_info['photo'] }}"><br>
+                <img class='photo' src="{{ config('services.dictorpus.url').$book_info['photo'] }}">
         @endif
-            <p>{{ $book_info['title'] }}</p>
+                <p class="book-title">{{ $book_info['title'] }}</p>
+            </div>
         </div>
     @endforeach
     </div>
